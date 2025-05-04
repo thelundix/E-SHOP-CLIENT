cacd0d5720d6d490046693f889535bb02adca0ce
Cabbe <121824429+thelundix@users.noreply.github.com>
2025-05-04 17:58:45 +0200

    Update main.js

--- client/src/main.js
+++ client/src/main.js
@@ -1,15 +1,11 @@
-// src/main.js
 import { renderCustomers } from './features/customers/customers.js';
 import { renderProducts } from './features/products/products.js';
 import { renderSuppliers } from './features/suppliers/suppliers.js';
-
-
-
 
 document.getElementById('show-products').addEventListener('click', renderProducts);
 document.getElementById('show-suppliers').addEventListener('click', renderSuppliers);
 document.getElementById('show-customers').addEventListener('click', renderCustomers);
 
 
-// Initial load
+
 renderProducts();
